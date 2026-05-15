64e148aa5af8e0a4cfe251db42118e466db54f03
Anonymous <a97928012@gmail.com>
2026-05-15 20:33:42 +0100

    Completed controller for tasks
    - Completed taskController.js which performs CRUD operations based on the request and route set previously in taskRouter.js

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,23 +1,84 @@
 const pool = require('../db');
 
 const createTask = async (req, res) => {
-    console.log("Created");
+    try {
+        const { title, description, status, due_date} = req.body
+        if(!title||!status||!due_date){
+            return res.status(400).json({ error: 'All fields are required' });
+        }
+        const result = await pool.query(
+            'INSERT INTO tasks (title, description, status, due_date) VALUES ($1,$2,$3,$4) RETURNING *'
+            ,[title, description, status, due_date]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 const getTasks = async (req, res) => {
-    console.log("Tasks");
+    try {
+        const result = await pool.query(
+            'SELECT * FROM tasks'
+        );
+        res.json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 const getTaskByID = async (req, res) => {
-    console.log("Task x");
+    try {
+        const {id} = req.params;
+        const result = await pool.query(
+            'SELECT * FROM tasks WHERE id=$1',
+            [id]
+        );
+        if(result.rows.length === 0) {
+            return res.status(404).json({error:'Task not found'});
+        }
+        res.json(result.rows[0])
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 const updateTask = async (req, res) => {
-    console.log("Task x updated");
+    try {
+        const {id} = req.params;
+        const {title, description, status, due_date} = req.body;
+        if(!title||!status||!due_date){
+            return res.status(400).json({ error: 'All fields are required' });
+        }
+
+        const result = await pool.query(
+            'UPDATE tasks SET title=$1, description=$2, status=$3, due_date=$4 WHERE id=$5 RETURNING *',
+            [title, description, status, due_date, id]
+        ); 
+
+        if(result.rows.length === 0) {
+            return res.status(404).json({error:'Task not found'});
+        }
+        res.json(result.rows[0]);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 const deleteTask = async (req, res) => {
-    console.log("Task x deleted");
+        try {
+        const {id} = req.params;
+        const result = await pool.query(
+            'DELETE * FROM tasks WHERE id=$1 returning *',
+            [id]
+        );
+        if(result.rows.length === 0) {
+            return res.status(404).json({error:'Task not found'});
+        }
+         res.json({ message: 'Task deleted', task: result.rows[0] });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 };
 
 module.exports = {
